98c47c07caad25a3d348ac5af2ef6a36f30fe20f
koumi15cancer <daiduong040700@gmail.com>
2024-02-22 09:29:28 +0800

    function getURLsFromHTML and test cases

--- crawl.js
+++ crawl.js
@@ -1,3 +1,6 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
 function normalizeURL(urlString) {
   try {
     const urlObj = new URL(urlString)
@@ -9,9 +12,39 @@ function normalizeURL(urlString) {
   } catch (err) {
     throw err;
   }
-}
+};
+
+function getURLsFromHTML(htmlBody, baseURL) {
+
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const aElements = dom.window.document.querySelectorAll('a');
+
+    for (const aElement of aElements ) {
+      if (aElement.href.slice(0,1) ==='/') {
+           try {
+            urls.push(new URL(aElement.href,baseURL).href);
+           } catch (error) {
+            console.log(`${err.message}: ${aElement.href}`)
+           }
+
+      } else {
+        try {
+          urls.push(new URL(aElement.href).href);
+         } catch (error) {
+          console.log(`${error.message}: ${aElement.href}`)
+         }
+
+      }
+
+    };
+    return urls
+
+};
+
 
 module.exports = {
-  normalizeURL
+  normalizeURL,
+  getURLsFromHTML
 };
 
